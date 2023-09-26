6535f940b81372f399b05dfcca51ace7919575be
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-09-26 11:45:57 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map((num) => num * 2);
+
+console.log(doubledNumbers);
+
+const words = ["Hello", "World", "JavaScript", "TypeScript", "Programming"];
+
+const filteredWords = words.filter((word) => word.length > 5);
+
+console.log(filteredWords);
+
+const numbers1 = [1, 2, 3, 4, 5];
+
+const sum = numbers1.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+console.log(sum);
+
+const numbers2 = [5, 8, 15, 3, 2];
+
+const hasNumberGreaterThanTen = numbers2.some((num) => num > 10);
+
+console.log(hasNumberGreaterThanTen);
